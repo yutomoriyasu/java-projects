1e56450f8a01772edefba629bb5025f3ad7b14e3
Yuto Moriyasu <y.moriyasu@icloud.com>
2022-07-13 14:26:45 +0900

    solve problem 1

---
+++ assignment6/IntegralSolver.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.util.*;
+
+public class IntegralSolver {
+
+    public static final int SOL_TRA = 1;
+    public static final int SOL_GAU = 2;
+
+    public static void main(String[] args) {
+
+        IntegralSolver solver = new IntegralSolver();
+        try {
+            // 積分方法を指定する部分
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Trapezoidal rule > Put 1");
+            System.out.println("Gaussian quadrature > Put 2");
+            System.out.print("How to solve? >");
+            System.out.flush();
+            int sol = Integer.parseInt(br.readLine());
+
+            // 積分方法毎に呼び出す関数を選択
+            double ans = 0.0;
+            if (sol == SOL_TRA) {
+                ans = solver.trapezoidal_solve(-1.0, 1.0);
+            } else if (sol == SOL_GAU) {
+                ans = solver.gauss_solve(-1.0, 1.0);
+            }
+            
+            System.out.println("Integration = " + ans);
+
+            // 例外処理の記述
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+        }
+    }
+
+    // aからbまでの関数f(x)の台形積分結果を返す
+    public double trapezoidal_solve(double a, double b) {
+        //
+        //　課題部分 1
+        //
+
+
+
+    }
+
+    // aからbまでの関数f(x)の2点のガウス積分結果を返す
+    public double gauss_solve(double a, double b) {
+        //
+        //　課題部分 2
+        //
+
+
+
+    }
+
+    // exp(x) を返す
+    // 使わなくてもよい
+    public double fun(double x) {
+        return Math.exp(x);
+    }
+
+}
---
+++ assignment6/SinSolver.java
@@ -0,0 +1,77 @@
+import java.io.*;
+import java.util.*;
+
+public class SinSolver {
+
+    // 積分方法を指定するための定数
+    private static final int SOL_2P = 1;
+    private static final int SOL_4P = 2;
+
+    public static void main(String[] args) {
+
+        // 積分方法毎に異なるインスタンスを生成
+        SinSolver solver = new SinSolver();
+
+        try {
+            // 積分方法を指定する部分
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("2 points Gauss > Put 1");
+            System.out.println("4 points Gauss > Put 2");
+            System.out.print("How to solve? >");
+            System.out.flush();
+            int sol = Integer.parseInt(br.readLine());
+
+            double ans = 0.0;
+            if (sol == SOL_2P) {
+                ans = solver.solve2point();
+            } else if (sol == SOL_4P) {
+                ans = solver.solve4point();
+            } 
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+        }
+    }
+
+
+    private double DOWN = 0;
+    private double UP = Math.PI / 2;
+
+    private static final double X_4_1 = 0.861136311;
+    private static final double X_4_2 = 0.339981043;
+
+    private static final double ALPHA_4_1 = 0.347854845;
+    private static final double ALPHA_4_2 = 0.652145154;
+
+    private static final double X_2 = 0.577350269;
+
+    private static final double ALPHA_2 = 1.0;
+
+
+    public double solve2point() {
+        //
+        // 課題 1
+        //
+        
+
+
+    }
+
+    public double solve4point() {
+        //
+        // 課題 2
+        //
+
+    }
+
+    // sin(x) を返す　使わなくても良い
+    public double fun(double x) {
+        double fx = Math.sin(x);
+        return fx;
+    }
+
+    // 変数変換をsolve4pint()などの中で行う場合はこの関数は使わなくてもよい
+    private double changeRange(double x) {
+            double changeVariable = ((UP - DOWN) * x / 2) + ((UP - DOWN) / 2);
+            return changeVariable;
+    }
+}
