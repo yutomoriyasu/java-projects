48e5baa10ecc98802cf8b9e1722a129eb02b6305
Yuto Moriyasu <y.moriyasu@icloud.com>
2022-07-20 15:31:09 +0900

    solve three step Runge-Kutta

--- assignment7/ThreestepRungeKuttaSolver.java
+++ assignment7/ThreestepRungeKuttaSolver.java
@@ -7,11 +7,17 @@ class ThreestepRungeKuttaSolver extends OrdinaryDifferentialSolver{
     //OrdinaryDifferentialSolverで定義されている変数を使うこと
     //public void set() はOrdinaryDifferentialSolverのものが呼び出される
     public void solve(){
-        /*ここから記入してください
-
-
-
-
-        ここまで記入してください*/
+        //ここから記入してください
+        int i = 0;
+        x[0] = 0 + h;
+        while (x[i] <= n) {
+            double k1 = h * y[i];
+            double k2 = h * (y[i] + k1/2);
+            double k3 = h * (y[i] - k1 + 2 * k2);
+            y[i+1] = y[i] + (k1 + 4 * k2 + k3) / 6;
+            x[i+1] = x[i] + h;
+            i++;
+        }
+        //ここまで記入してください
     }
 }
