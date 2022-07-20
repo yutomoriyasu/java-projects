22a61bd2a3011164908793665a35626da61df6e0
Yuto Moriyasu <y.moriyasu@icloud.com>
2022-07-20 15:26:57 +0900

    solve two step Runge-Kutta

--- assignment7/TwostepRungeKuttaSolver.java
+++ assignment7/TwostepRungeKuttaSolver.java
@@ -6,11 +6,16 @@ class TwostepRungeKuttaSolver extends OrdinaryDifferentialSolver{
     //OrdinaryDifferentialSolverで定義されている変数を使うこと
     //public void set() はOrdinaryDifferentialSolverのものが呼び出される
     public void solve(){
-        /*ここから記入してください
-
-
-
-
-        ここまで記入してください*/
+        //ここから記入してください
+        int i = 0;
+        x[0] = 0 + h;
+        while (x[i] <= n) {
+            double k1 = h * y[i];
+            double k2 = h * (y[i] + k1);
+            y[i+1] = y[i] + (k1 + k2) / 2;
+            x[i+1] = x[i] + h;
+            i++;
+        }
+        //ここまで記入してください
     }    
 }
