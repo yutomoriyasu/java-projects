cb36ccb13cc4ca7a91b6896e213f84d8e9abd305
Yuto Moriyasu <y.moriyasu@icloud.com>
2022-07-20 15:20:16 +0900

    solve modified euler

--- assignment7/ModifiedEulersFormulaSolver.java
+++ assignment7/ModifiedEulersFormulaSolver.java
@@ -5,11 +5,16 @@ class ModifiedEulersFormulaSolver extends OrdinaryDifferentialSolver{
     //OrdinaryDifferentialSolverで定義されている変数を使うこと
     //public void set() はOrdinaryDifferentialSolverのものが呼び出される
     public void solve(){
-        /*ここから記入してください
-
-
-
+        //ここから記入してください
+        int i = 0;
+        x[0] = 0 + h;
+        while (x[i] <= n) {
+            double k1 = h * y[i];
+            x[i+1] = x[i] + h;
+            y[i+1] = y[i] + h * (y[i] + k1/2);
+            i++;
+        }
         
-        ここまで記入してください*/
+        //ここまで記入してください
     }
 }
